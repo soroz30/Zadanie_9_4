1445048d6f12c5dbba0047ff72e751fc826938bd
soroz30 <soroz30@yahoo.pl>
2017-09-27 17:29:01 +0200

    correction

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 function drawTree(stars) {
-	for (var i = 1; i <= stars; i++) {
-		var star = ' '.repeat(stars - i);
-		for (var j = 1; j <= i; j++) {
-			star += '* ';
-		}
-		console.log(star);
-	}
+    for (var i = 1; i <= stars; i++) {
+        var star = ' '.repeat(stars - i);
+        for (var j = 1; j <= i; j++) {
+            star += '* ';
+        }
+        console.log(star);
+    }
 }
 
 drawTree(3);
